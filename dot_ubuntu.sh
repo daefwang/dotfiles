6a6023b7549a5dc496ebde4133a10a019ad2257e
DaeHwan Kim <eoghks319@gmail.com>
2022-08-05 19:28:29 +0900

    update conda version

--- dot_ubuntu.sh
+++ dot_ubuntu.sh
@@ -49,4 +49,4 @@ git config --global core.editor "code --wait"
 git config --global alias.lg "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
 
 # Download anaconda script
-wget https://repo.anaconda.com/archive/Anaconda3-2020.11-Linux-x86_64.sh
+wget https://repo.anaconda.com/archive/Anaconda3-2022.05-Linux-x86_64.sh
